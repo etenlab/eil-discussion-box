eb642e928b2e819ab6afea853598c3fee36de153
hiroshi <hiroshi.tan57@gmail.com>
2022-12-13 21:46:50 +0000

    Add example for discussion-box

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { DiscussionPage } from "./components/DiscusssionPage";
+
+function App() {
+  return (
+    <Router>
+      <Route path="/discussion/:table_name/:row" component={DiscussionPage} />
+    </Router>
+  );
+}
+
+export default App;
---
+++ example/src/components/DiscusssionPage/DiscussionPage.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { useHistory, useParams } from "react-router";
+
+import { Stack, Button } from "@mui/material";
+
+import {
+  Discussion,
+  DiscussionHeader,
+  MockLoginForm,
+  DiscussionRouteQuizParams,
+} from "@eten-lab/discussion-box";
+
+function existsAll(
+  userId: number | null,
+  tableName: string | null,
+  rowId: number | null
+) {
+  if (userId !== null && tableName !== null && rowId !== null) {
+    return true;
+  }
+  return false;
+}
+
+/**
+ * This component will mount once users route to '/tab1/discussion/:table_name/:row'.
+ * The responsibility is to control Discussion Page and interact with server such as fetching, saving, deleting discussion data.
+ */
+export function DiscussionPage() {
+  const history = useHistory();
+  const { table_name, row } = useParams<DiscussionRouteQuizParams>();
+
+  const [mockUserId, setMockUserId] = useState<number | null>(null);
+
+  const tableName = table_name || null;
+  const rowId = (row && row.length > 0) ? +row : null;
+
+  return (
+    <>
+      <Stack
+        justifyContent="space-between"
+        sx={{ height: "calc(100vh - 75px)", padding: "0px 20px" }}
+      >
+        <DiscussionHeader>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <span>Discussion</span>
+            <Stack direction="row" alignItems="center" spacing={2}>
+              <Button
+                onClick={() => {
+                  history.goBack();
+                }}
+              >
+                Go Back
+              </Button>
+            </Stack>
+          </Stack>
+        </DiscussionHeader>
+
+        <MockLoginForm
+          mockUserId={mockUserId}
+          setMockUserId={(userId: number) => setMockUserId(userId)}
+        />
+
+        {existsAll(mockUserId, tableName, rowId) && (
+          <Discussion userId={mockUserId!} tableName={tableName!} rowId={rowId!} />
+        )}
+      </Stack>
+    </>
+  );
+}
