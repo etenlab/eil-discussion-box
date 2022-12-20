4cf654e815d19d042c4399d8755b126749c8058e
hiroshi <hiroshi.tan57@gmail.com>
2022-12-20 21:22:52 +0000

    Add aggregation-api graphql client

---
+++ packages/discussion-box/src/graphql/aggregationGraphql.ts
@@ -0,0 +1,22 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
+
+import { typeDefs } from "./discussionQuery";
+
+export const aggregationClient = new ApolloClient({
+  link: createUploadLink({
+    uri: process.env.REACT_APP_AGGREGATION_API,
+  }),
+  cache: new InMemoryCache(),
+
+  // Provide some optional constructor fields
+  name: "react-web-client",
+  version: "1.3",
+  queryDeduplication: false,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "cache-and-network",
+    },
+  },
+  typeDefs,
+});
