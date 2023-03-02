913ec9537604b8bbf4e1c69a7728bd84ace9410f
hiroshi <hiroshi.tan57@gmail.com>
2023-03-02 23:19:26 +0000

    Refactor ReactionList

--- packages/discussion-box/src/Reaction/index.ts
+++
@@ -1,4 +0,0 @@
-export { Reaction } from "./Reaction";
-export { ReactionList } from "./ReactionList";
-export { AddReactionButton } from "../common/AddReactionButton";
-export { TooltipContent } from "./TooltipContent";
