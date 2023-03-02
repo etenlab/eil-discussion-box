b61b1dc2cd50afba55a22daa3576231c363ce301
hiroshi <hiroshi.tan57@gmail.com>
2023-03-02 23:18:08 +0000

    Refactor Post with ui-kit

--- packages/discussion-box/src/Post/index.ts
+++
@@ -1,6 +0,0 @@
-export { Post } from "./Post";
-export { ActionList } from "./ActionList";
-export { DateViewer } from "./DateViewer";
-export { PostHeader } from "./PostHeader";
-export { PostList } from "./PostList";
-export { Username } from "./Username";
